755ebed3f7cfe4f93300adaf1439eaf102c22cb5
mehmetoztokay <mehmetoztokay@gmail.com>
2025-11-20 12:33:54 +0300

    Add modal

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import {useState} from "react";
+import {Modal, Pressable, StyleSheet, Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Pressable onPress={() => setModalVisible(true)}>
+          <Text>Press Me</Text>
+        </Pressable>
+
+        <Modal visible={modalVisible} animationType="slide">
+          <SafeAreaView style={{flex: 1, backgroundColor: "#cecece"}}>
+            <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+              <Text>Modal is open!</Text>
+              <Pressable style={{backgroundColor: "purple", padding: 5, borderRadius: 5}} onPress={() => setModalVisible(false)}>
+                <Text style={{color: "white"}}>Close Modal</Text>
+              </Pressable>
+            </View>
+          </SafeAreaView>
+        </Modal>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fefefe",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
